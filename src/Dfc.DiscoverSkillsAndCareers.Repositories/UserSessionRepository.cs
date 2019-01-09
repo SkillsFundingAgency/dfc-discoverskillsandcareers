c65d54239b160fa450309123d069f8668c682132
Oliver Booth <obooth@preppygreen.com>
2019-01-09 14:26:45 +0000

    Repositories project

---
+++ src/Dfc.DiscoverSkillsAndCareers.Repositories/UserSessionRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading.Tasks;
+using Dfc.DiscoverSkillsAndCareers.Models;
+
+namespace Dfc.DiscoverSkillsAndCareers.Repositories
+{
+    public class UserSessionRepository
+    {
+        public async Task<UserSession> GetUserSessionAsync(string sessionId)
+        {
+            return new UserSession()
+            {
+                SessionId = sessionId
+            };
+        }
+    }
+}
