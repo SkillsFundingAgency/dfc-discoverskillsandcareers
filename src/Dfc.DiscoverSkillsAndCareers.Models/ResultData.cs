5829a22caebae3a6edf8de4ca6dcc95ed8011ab7
Oliver Booth <obooth@preppygreen.com>
2019-01-11 14:14:26 +0000

    Models

---
+++ src/Dfc.DiscoverSkillsAndCareers.Models/ResultData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace Dfc.DiscoverSkillsAndCareers.Models
+{
+    public class ResultData
+    {
+        [JsonProperty("traitCodes")]
+        public List<string> TraitCodes { get; set; }
+        [JsonProperty("jobFamilyCodes")]
+        public List<string> JobFamilyCodes { get; set; }
+    }
+}
