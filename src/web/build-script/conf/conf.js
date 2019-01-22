42c95c3886d099c6d32196b321932fc1ff35f8ee
Gohar Iqbal <gohar.iqbal@infinityworks.com>
2019-01-22 09:49:42 +0000

    Added more browsers to match gov requirement

--- src/web/build-script/conf/conf.js
+++ src/web/build-script/conf/conf.js
@@ -39,6 +39,39 @@ exports.config = {
         'browserName': 'Firefox',
         'browser_version': '65.0 beta',
         'resolution': '1024x768'
+    },
+    {
+        'os': 'OS X',
+        'os_version': 'Mojave',
+        'browserName': 'Safari',
+        'browser_version': '12.0',
+        'resolution': '1024x768'
+    },
+    {
+        'os': 'OS X',
+        'os_version': 'Mojave',
+        'browserName': 'Chrome',
+        'browser_version': '72.0 beta',
+        'resolution': '1024x768'
+    },
+    {
+        'os': 'OS X',
+        'os_version': 'Mojave',
+        'browserName': 'Firefox',
+        'browser_version': '65.0 beta',
+        'resolution': '1024x768'
+    },
+    {
+        'browserName' : 'iPhone',
+        'device': 'iPhone XS',
+        'realMobile': 'true',
+        'os_version': '12.1'
+    },
+    {
+        'browserName' : 'android',
+        'device': 'Samsung Galaxy S9',
+        'realMobile': 'true',
+        'os_version': '8.0'
     }],
 
     // Code to start browserstack local before start of test
