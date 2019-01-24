7d719cf751108104e52861c57e953e7142d4f2ee
Oliver Booth <obooth@preppygreen.com>
2019-01-24 17:17:10 +0000

    Results display limit

--- src/Dfc.DiscoverSkillsAndCareers.FunctionApp/Results/BuildPageHtml.cs
+++ src/Dfc.DiscoverSkillsAndCareers.FunctionApp/Results/BuildPageHtml.cs
@@ -10,12 +10,19 @@ namespace Dfc.DiscoverSkillsAndCareers.FunctionApp.Results
         public BuildPageHtml(string html, SessionHelper sessionHelper)
         {
             var jobFamilyHtml = "";
+            int displayCounter = 0;
+            bool displayAll = false;
             sessionHelper.Session.ResultData.JobFamilies.ForEach(jobFamily =>
             {
-                jobFamilyHtml += $"<li class=\"app-result\"><h3 class=\"govuk-heading-m app-result__header\">{jobFamily.JobFamilyName}</h3><div class=\"app-result__body\"><p>{jobFamily.JobFamilyText}</p><a href=\"{jobFamily.Url}\" role=\"button\" draggable=\"false\" class=\"govuk-button govuk-button--start app-button--alt\">See job category</a></div></li>";
+                if (displayCounter < 3 || displayAll)
+                {
+                    jobFamilyHtml += $"<li class=\"app-result\"><h3 class=\"govuk-heading-m app-result__header\">{jobFamily.JobFamilyName}</h3><div class=\"app-result__body\"><p>{jobFamily.JobFamilyText}</p><a href=\"{jobFamily.Url}\" role=\"button\" draggable=\"false\" class=\"govuk-button govuk-button--start app-button--alt\">See job category</a></div></li>";
+                    displayCounter++;
+                }
             });
 
             var traitHtml = "";
+            displayCounter = 0;
             bool isLocal = string.IsNullOrEmpty(Environment.GetEnvironmentVariable("WEBSITE_INSTANCE_ID"));
             sessionHelper.Session.ResultData.Traits.ForEach(trait =>
             {
@@ -23,9 +30,10 @@ namespace Dfc.DiscoverSkillsAndCareers.FunctionApp.Results
                 {
                     traitHtml += $"<p>{trait.TraitName} {trait.TotalScore} {trait.TraitText}</p>";
                 }
-                else
+                else if (displayCounter < 3)
                 {
                     traitHtml += $"<p>{trait.TraitText}</p>";
+                    displayCounter++;
                 }
             });
 
@@ -41,7 +49,8 @@ namespace Dfc.DiscoverSkillsAndCareers.FunctionApp.Results
             html = html.Replace("[job_families_li_html]", jobFamilyHtml);
             html = html.Replace("[traits_li_html]", traitHtml);
             html = html.Replace("[static_site_domain]", sessionHelper.Config.StaticSiteDomain);
-            html = html.Replace("[job_family_count]", sessionHelper.Session.ResultData.JobFamilies.Count);
+            html = html.Replace("[job_family_count]", sessionHelper.Session.ResultData.JobFamilies.Count.ToString());
+            html = html.Replace("[job_family_more_count]", (sessionHelper.Session.ResultData.JobFamilies.Count - 3).ToString());
             Html = html;
         }
     }
