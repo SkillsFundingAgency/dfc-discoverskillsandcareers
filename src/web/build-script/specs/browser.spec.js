c2b9175a7f28f322f2a7824419c477b1313dfef0
Gohar Iqbal <gohar.iqbal@infinityworks.com>
2019-01-24 12:08:58 +0000

    Updated link for browserstack test

--- src/web/build-script/specs/browser.spec.js
+++ src/web/build-script/specs/browser.spec.js
@@ -1,6 +1,6 @@
 describe('National Careers Service', function() {
   it('Find title of question page', function() {
-    browser.driver.get('https://ncstestdiscoverskills.z6.web.core.windows.net/').then(function() {
+    browser.driver.get('https://dfcdevskillscareersstr.z6.web.core.windows.net/').then(function() {
       browser.driver.findElement(By.linkText('Start assessment')).click().then(() => {
         browser.driver.findElement(By.linkText('Start now')).click().then(() => {
           expect(browser.driver.getTitle()).toEqual('National Careers Service');
