3f6f73db7c73f2cbaf05db41fbc1e3c4fb25baf2
Nick Graham101 <nick.graham101@googlemail.com>
2019-02-04 15:48:05 +0000

    Removed cmdlet verbose preferences

--- Resources/PSScripts/Set-CdnKeyVaultAccess.ps1
+++ Resources/PSScripts/Set-CdnKeyVaultAccess.ps1
@@ -60,7 +60,7 @@ $CdnServicePrincipal = Get-AzureADServicePrincipal -SearchString Microsoft.Azure
 if (!$CdnServicePrincipal) {
 
     Write-Verbose -Message "Service Principle not registered for Microsoft.Azure.Cdn, registering ..."
-    $CdnServicePrincipal = New-AzureADServicePrincipal -AppId "205478c0-bd83-4e1b-a9d6-db63a3e1e1c8" -AccountEnabled $true -Tags {WindowsAzureActiveDirectoryIntegratedApp} $VerbosePreference
+    $CdnServicePrincipal = New-AzureADServicePrincipal -AppId "205478c0-bd83-4e1b-a9d6-db63a3e1e1c8" -AccountEnabled $true -Tags {WindowsAzureActiveDirectoryIntegratedApp}
 
 }
 
@@ -71,7 +71,7 @@ if ($KeyVault) {
     if (!$CdnSpnKeyVaultAccessPolicy) {
 
         Write-Verbose -Message "Setting KeyVault access policy for CDN Service Principal"
-        Set-AzureRmKeyVaultAccessPolicy -VaultName  $KeyVaultName -ResourceGroupName $KeyVaultResourceGroup -ServicePrincipalName $CdnServicePrincipal.ServicePrincipalNames[0] -PermissionsToSecrets get $VerbosePreference
+        Set-AzureRmKeyVaultAccessPolicy -VaultName  $KeyVaultName -ResourceGroupName $KeyVaultResourceGroup -ServicePrincipalName $CdnServicePrincipal.ServicePrincipalNames[0] -PermissionsToSecrets get
 
     }
     else {
