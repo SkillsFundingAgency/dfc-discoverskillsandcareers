33a9372603c3113e39e7802ba51d1a9690811bcd
Nick Graham101 <nick.graham101@googlemail.com>
2019-02-04 16:06:05 +0000

    Fixed error in access policy where cmd, improved logging

--- Resources/PSScripts/Set-CdnKeyVaultAccess.ps1
+++ Resources/PSScripts/Set-CdnKeyVaultAccess.ps1
@@ -60,18 +60,18 @@ $CdnServicePrincipal = Get-AzureADServicePrincipal -SearchString Microsoft.Azure
 if (!$CdnServicePrincipal) {
 
     Write-Verbose -Message "Service Principle not registered for Microsoft.Azure.Cdn, registering ..."
-    $CdnServicePrincipal = New-AzureADServicePrincipal -AppId "205478c0-bd83-4e1b-a9d6-db63a3e1e1c8" -AccountEnabled $true -Tags {WindowsAzureActiveDirectoryIntegratedApp}
+    $CdnServicePrincipal = New-AzureADServicePrincipal -AppId "205478c0-bd83-4e1b-a9d6-db63a3e1e1c8" -AccountEnabled $true -Tags {WindowsAzureActiveDirectoryIntegratedApp} -Verbose:$VerbosePreference
 
 }
 
 $KeyVault = Get-AzureRmKeyVault -VaultName $KeyVaultName
 if ($KeyVault) {
 
-    $CdnSpnKeyVaultAccessPolicy = $KeyVault.AccessPolicies | Where-Object { $_.ObjectId -eq $CdnServicePrincipal.Id }
+    $CdnSpnKeyVaultAccessPolicy = $KeyVault.AccessPolicies | Where-Object { $_.ObjectId -eq $CdnServicePrincipal.ObjectId }
     if (!$CdnSpnKeyVaultAccessPolicy) {
 
-        Write-Verbose -Message "Setting KeyVault access policy for CDN Service Principal"
-        Set-AzureRmKeyVaultAccessPolicy -VaultName  $KeyVaultName -ResourceGroupName $KeyVaultResourceGroup -ServicePrincipalName $CdnServicePrincipal.ServicePrincipalNames[0] -PermissionsToSecrets get
+        Write-Verbose -Message "Setting KeyVault access policy for CDN Service Principal: $($CdnServicePrincipal.ServicePrincipalNames[1])"
+        Set-AzureRmKeyVaultAccessPolicy -VaultName  $KeyVaultName -ResourceGroupName $KeyVaultResourceGroup -ServicePrincipalName $CdnServicePrincipal.ServicePrincipalNames[1] -PermissionsToSecrets get -Verbose:$VerbosePreference
 
     }
     else {
