95d40ec0e7cc50cb64ddf9d73dac7380fc00bb3a
Oliver Booth <obooth@preppygreen.com>
2019-02-04 10:17:36 +0000

    Refactor sessionId to contain partitionKey, further tests

--- src/Dfc.DiscoverSkillsAndCareers.FunctionApp/Helpers/SessionIdHelper.cs
+++ src/Dfc.DiscoverSkillsAndCareers.FunctionApp/Helpers/SessionIdHelper.cs
@@ -5,12 +5,56 @@ namespace Dfc.DiscoverSkillsAndCareers.FunctionApp.Helpers
 {
     public static class SessionIdHelper
     {
-        public static string GenerateSessionId(string salt)
+        private const string Alphabet = "acefghjkmnrstwxyz23456789";
+
+        public static string GenerateSessionId(string salt, DateTime date)
         {
-            var hashids = new Hashids(salt, 4, "acefghjkmnrstwxyz23456789");
-            long digits = Convert.ToInt64(DateTime.Now.ToString("yyMMddHHmmssf"));
-            var hash = hashids.EncodeLong(digits);
-            return hash;
+            var hashids = new Hashids(salt, 4, Alphabet);
+            int rand = Counter();
+            string year = (date.Year - 2018).ToString();
+            long digits = Convert.ToInt64($"{year}{date.ToString("MMddHHmmssfff")}{rand}");
+            var code = hashids.EncodeLong(digits);
+            var decode = Decode(salt, code);
+            if (digits.ToString() != decode.ToString())
+            {
+                throw new Exception("Invalid decode");
+            }
+            return code;
+        }
+
+        public static string GenerateSessionId(string salt) => GenerateSessionId(salt, DateTime.UtcNow);
+
+        public static string Decode(string salt, string code)
+        {
+            var hashids = new Hashids(salt, 4, Alphabet);
+            return hashids.DecodeLong(code)?[0].ToString();
+        }
+
+        public static string GetYearMonth(string datetimeStamp)
+        {
+            int yearDigit;
+            if (int.TryParse(datetimeStamp.Substring(0, 1), out yearDigit))
+            {
+                int year = yearDigit + 2018;
+                int month;
+                if (int.TryParse(datetimeStamp.Substring(1, 2), out month))
+                {
+                    return new DateTime(year, month, 1).ToString("yyyyMM");
+                }
+            }
+            return null;
+        }
+
+        private static int _counter = 10;
+        private static readonly object _syncLock = new object();
+        public static int Counter()
+        {
+            lock (_syncLock)
+            {
+                if (_counter >= 99) _counter = 0;
+                _counter++;
+                return _counter;
+            }
         }
     }
 }
