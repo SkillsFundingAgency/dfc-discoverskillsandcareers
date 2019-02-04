89a86c9310894243c37ccb57285e2623b01c9117
Nick Graham101 <nick.graham101@googlemail.com>
2019-02-04 14:33:50 +0000

    Refactored to connect to AAD with SPN

--- Resources/PSScripts/Set-CdnKeyVaultAccess.ps1
+++ Resources/PSScripts/Set-CdnKeyVaultAccess.ps1
@@ -3,14 +3,53 @@ param(
     [Parameter(Mandatory=$true)]
     [string]$KeyVaultName,
     [Parameter(Mandatory=$true)]
-    [string]$KeyVaultResourceGroup
+    [string]$KeyVaultResourceGroup,
+    [Parameter(Mandatory=$true)]
+    [string]$TenantId,
+    [Parameter(Mandatory=$true)]
+    [string]$EndpointClientId,
+    [Parameter(Mandatory=$true)]
+    [string]$EndpointClientSecret
 )
 
-$CdnServicePrincipal = Get-AzureRmADServicePrincipal -DisplayName Microsoft.Azure.Cdn
+try {
+
+    if(!($ENV:TF_BUILD)) {
+
+        Connect-AzureAD -TenantId $TenantId
+    
+    }
+    else {
+    
+        $AADTokenUrl = "https://login.microsoftonline.com/$TenantId/oauth2/token"
+        
+        $Body = @{
+            grant_type    = "client_credentials"
+            client_id     = $EndpointClientId
+            client_secret = $EndpointClientSecret
+            resource      = "https://graph.windows.net/"
+        }
+        
+        $Response = Invoke-RestMethod -Method POST -Uri $AADTokenUrl -ContentType "application/x-www-form-urlencoded" -Body $Body
+        $Token = $Response.access_token
+        
+        Write-Verbose "Login to AzureAD with same application as endpoint"
+        $null = Connect-AzureAD -AadAccessToken $Token -AccountId $EndpointClientId -TenantId $TenantId
+    
+    }
+
+}
+catch {
+
+    throw "ERROR: unable to login to Active Directory tenant $TenantId with app registration ApplicationId $EndpointClientId`n$($Error[0])"
+
+}
+
+$CdnServicePrincipal = Get-AzureADServicePrincipal -SearchString Microsoft.Azure.Cdn
 if (!$CdnServicePrincipal) {
 
     Write-Verbose -Message "Service Principle not registered for Microsoft.Azure.Cdn, registering ..."
-    $CdnServicePrincipal = New-AzureRmADServicePrincipal -ApplicationId "205478c0-bd83-4e1b-a9d6-db63a3e1e1c8"
+    $CdnServicePrincipal = New-AzureADServicePrincipal -ApplId "205478c0-bd83-4e1b-a9d6-db63a3e1e1c8" -AccountEnabled $true -Tags {WindowsAzureActiveDirectoryIntegratedApp}
 
 }
 
