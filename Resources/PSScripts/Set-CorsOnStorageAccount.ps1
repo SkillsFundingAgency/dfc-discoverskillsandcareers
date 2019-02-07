3a86bcbad31baaa1d6b176460baae9991f16b1fa
NickGraham101 <nick.graham101@googlemail.com>
2019-02-07 09:30:17 +0000

    Added script to set CORS rules on storage

---
+++ Resources/PSScripts/Set-CorsOnStorageAccount.ps1
@@ -0,0 +1,38 @@
+[CmdletBinding()]
+param(
+    [Parameter(Mandatory = $true)]
+    [string]$StorageAccountName,
+    [Parameter(Mandatory = $true)]
+    [string[]]$AllowedOrigins
+)
+
+Write-Verbose -Message "Setting Storage Context"
+$StorageContext = New-AzureStorageContext -StorageAccountName $StorageAccountName -UseConnectedAccount
+
+# ---- CORS Settings
+foreach ($AllowedOrigin in $AllowedOrigins) {
+
+    $CORSRules = (@{
+        AllowedHeaders  = @("*");
+        AllowedOrigins  = @("$AllowedOrigin");
+        MaxAgeInSeconds = 3600;
+        AllowedMethods  = @("Get")
+    })
+    
+    try {
+        
+        # ---- Set CORS Rules
+        Write-Verbose -Message "Setting CORS rule on $($StorageContext.BlobEndPoint)"
+        Set-AzureStorageCORSRule -ServiceType Blob -CorsRules $CORSRules -Context $StorageContext
+    
+    }
+    catch {
+    
+        throw "Failed to get Storage Context and set CORS settings: $_"
+    
+    }
+
+}
+
+
+
