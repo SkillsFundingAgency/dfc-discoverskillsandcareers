d8c006303784a30c58aa6c1122b931707b508aaf
Colin Bull <colinbul@gmail.com>
2019-02-15 15:41:43 +0000

    Added extensions to get IoC regsitered

---
+++ src/Dfc.DiscoverSkillsAndCareers.FunctionApp/Startup.cs
@@ -0,0 +1,28 @@
+using DFC.Common.Standard.Logging;
+using DFC.Functions.DI.Standard;
+using DFC.HTTP.Standard;
+using DFC.JSON.Standard;
+using DFC.Swagger.Standard;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: WebJobsStartup(typeof(Dfc.DiscoverSkillsAndCareers.FunctionApp.WebJobsExtensionStartup), "Web Jobs Extension Startup")]
+
+namespace Dfc.DiscoverSkillsAndCareers.FunctionApp
+{
+    public class WebJobsExtensionStartup : IWebJobsStartup
+    {
+        public void Configure(IWebJobsBuilder builder)
+        {
+            builder.AddDependencyInjection();
+
+            builder.Services.AddSingleton<ILoggerHelper, LoggerHelper>();
+            builder.Services.AddSingleton<IHttpRequestHelper, HttpRequestHelper>();
+            builder.Services.AddSingleton<IHttpResponseMessageHelper, HttpResponseMessageHelper>();
+            builder.Services.AddSingleton<IJsonHelper, JsonHelper>();
+
+            builder.Services.AddScoped<ISwaggerDocumentGenerator, SwaggerDocumentGenerator>();
+        }
+    }
+}
