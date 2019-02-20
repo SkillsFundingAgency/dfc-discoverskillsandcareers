a9a0d33a07bc343a76422c4a508de949e216e28b
Oliver Booth <obooth@preppygreen.com>
2019-02-20 13:24:17 +0000

    Mvc models with content fields

--- src/Dfc.DiscoverSkillsAndCareers.WebApp/Models/IndexViewModel.cs
+++ src/Dfc.DiscoverSkillsAndCareers.WebApp/Models/IndexViewModel.cs
@@ -4,5 +4,16 @@
     {
         public string SessionId { get; set; }
         public bool HasReloadError { get; set; }
+        public string Title { get; set; } = "National Careers Service";
+        public string Headline { get; set; } = "Discover your skills and careers";
+        public string Subheading { get; set; } = "Take a quick assessment to see job categories you might be suited to. Or take a longer assessment to see specific job roles.";
+        public string Assessment1Title { get; set; } = "See job categories";
+        public string Assessment1Subtitle { get; set; } = "5 minutes";
+        public string Assessment2Title { get; set; } = "See specific job roles";
+        public string Assessment2Subtitle { get; set; } = "10 to 15 minutes";
+        public string ResumeTitle { get; set; } = "Resume your progress";
+        public string ResumeFieldTitle { get; set; } = "Reference number";
+        public string ResumeButtonText { get; set; } = "Resume progress";
+        public string ResumeErrorMessage { get; set; } = "The code could not be found";
     }
 }
--- src/Dfc.DiscoverSkillsAndCareers.WebApp/Models/QuestionViewModel.cs
+++ src/Dfc.DiscoverSkillsAndCareers.WebApp/Models/QuestionViewModel.cs
@@ -8,10 +8,19 @@
         public string TraitCode { get; set; }
         public string FormRoute { get; set; }
         public string SessionId { get; set; }
-        public string ButtonText { get; set; }
         public string ErrorMessage { get; set; }
         public string Percentage { get; set; }
         public string PercentrageLeft { get; set; }
         public string Code { get; set; }
+        public string Title { get; set; } = "National Careers Service - Statement";
+        public string SaveProgressLinkText { get; set; } = "Save my progress";
+        public string StronglyDisagree { get; set; } = "Strongly disagree";
+        public string StronglyAgree { get; set; } = "Strongly agree";
+        public string SaveProgressTitle { get; set; } = "Return to this later";
+        public string Neutral { get; set; } = "This doesn't apply to me";
+        public string Agree { get; set; } = "Agree";
+        public string NextButtonText { get; set; } = "Next";
+        public string Disagree { get; set; } = "Disagree";
+
     }
 }
