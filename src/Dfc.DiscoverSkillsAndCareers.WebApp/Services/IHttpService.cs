56f99a47c90bfb1fce453693ba5f25956b2264f8
Oliver Booth <obooth@preppygreen.com>
2019-02-20 16:49:10 +0000

    IHttpService, HttpService

---
+++ src/Dfc.DiscoverSkillsAndCareers.WebApp/Services/IHttpService.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+
+namespace Dfc.DiscoverSkillsAndCareers.WebApp.Services
+{
+    public interface IHttpService
+    {
+        Task<string> GetString(string url);
+        Task<string> PostData(string url, object data);
+    }
+}
