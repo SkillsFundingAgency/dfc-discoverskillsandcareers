45827e1823346c0338345f494bd1ec67b3584b76
Oliver Booth <obooth@preppygreen.com>
2019-03-04 16:40:14 +0000

    Filtering questions PoC tests

---
+++ src/Dfc.DiscoverSkillsAndCareers.Models/FilteringQuestion.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace Dfc.DiscoverSkillsAndCareers.Models
+{
+    public class FilteringQuestion
+    {
+        [JsonProperty("partitionKey")]
+        public string PartitionKey { get; set; }
+        [JsonProperty("id")]
+        public string FilteringQuestionId { get; set; }
+        [JsonProperty("texts")]
+        public QuestionText[] Texts { get; set; } = { };
+        [JsonProperty("jobFamily")]
+        public string JobFamily { get; set; }
+        [JsonProperty("order")]
+        public int Order { get; set; }
+        [JsonProperty("excludesJobProfiles")]
+        public List<string> ExcludesJobProfiles { get; set; } = new List<string>();
+        [JsonProperty("excludeAnswerTrigger")]
+        public string ExcludeAnswerTrigger { get; set; } = "No";
+        [JsonProperty("answerOptions")]
+        public List<string> AnswerOptions { get; set; } = new List<string> { "Yes", "No" };
+    }
+}
