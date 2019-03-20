cae9c2460db5940cf7591011e062337e2a9101ae
Oliver Booth <obooth@preppygreen.com>
2019-03-20 14:55:56 +0000

    Tests

---
+++ src/Dfc.IntegrationTests/RepositoryTests/JobProfileRepositoryTests.cs
@@ -0,0 +1,45 @@
+﻿using Dfc.DiscoverSkillsAndCareers.Models;
+using Dfc.DiscoverSkillsAndCareers.Repositories;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using NSubstitute;
+using System;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Dfc.IntegrationTests.RepositoryTests
+{
+    public class JobProfileRepositoryTests : IDisposable
+    {
+        public IConfigurationRoot Configuration { get; set; }
+
+        JobProfileRepository _repository;
+        ILogger<JobProfileRepository> _logger;
+        IOptions<CosmosSettings> _optsCosmosSettings;
+
+        public JobProfileRepositoryTests()
+        {
+            var builder = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: false);
+            Configuration = builder.Build();
+            var cosmosSettings = Configuration.Get<CosmosSettings>();
+            _optsCosmosSettings = Options.Create(cosmosSettings);
+            _logger = Substitute.For<ILogger<JobProfileRepository>>();
+            _repository = new JobProfileRepository(_optsCosmosSettings);
+        }
+
+        public void Dispose() { }
+
+        [Fact]
+        public async Task GetContent_WithValidKey_ShouldReturnJobProfile()
+        {
+            var socCode = "startpage";
+            var partitionKey = "jobprofiles-cms";
+
+            var jobProfile = await _repository.GetJobProfile(socCode, partitionKey);
+
+            Assert.Null(jobProfile);
+        }
+    }
+}
