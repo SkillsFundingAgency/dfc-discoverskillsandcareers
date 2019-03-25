a6c401be4d87710cb470077c89d168a69c5b3d1b
Oliver Booth <obooth@preppygreen.com>
2019-03-25 12:55:45 +0000

    Finish page content and cms

--- src/FunctionApps/Dfc.DiscoverSkillsAndCareers.CmsFunctionApp/Models/ContentFinishPage.cs
+++ src/FunctionApps/Dfc.DiscoverSkillsAndCareers.CmsFunctionApp/Models/ContentFinishPage.cs
@@ -5,11 +5,12 @@ namespace Dfc.DiscoverSkillsAndCareers.CmsFunctionApp.Models
 {
     public class ContentFinishPage : IContentPage
     {
-        public string Title { get; set; } = "National Careers Service - Finished";
-        public string Content { get; set; } = "On the next page you will see your results, which are based on the statements you have just responded to.";
+        public string Title { get; set; } = "Assessment complete | National Careers Service";
+        public string BreadcrumbText { get; set; } = "Assessment";
+        public string Content { get; set; } = "See the types of jobs that might suit you. Your results are based on your responses.";
         public string ContentTitle { get; set; } = "What to do next";
         public string Headline { get; set; } = "Assessment complete";
-        public string ViewResultsButtonText { get; set; } = "View results";
+        public string ViewResultsButtonText { get; set; } = "See results";
         public string HeadlineStrap { get; set; } = "Thank you";
         [JsonProperty("LastModified")]
         public DateTime LastUpdated { get; set; }
