140497bac1e312e11bcf1a248ef86ed71a1d75cd
Colin Bull <colinbul@gmail.com>
2019-04-03 17:10:01 +0100

    Fixing null reference exception

--- src/Dfc.DiscoverSkillsAndCareers.WebApp/Models/ResultsForJobCategoryViewModel.cs
+++ src/Dfc.DiscoverSkillsAndCareers.WebApp/Models/ResultsForJobCategoryViewModel.cs
@@ -3,7 +3,7 @@
     public class ResultsForJobCategoryViewModel
     {
         public string SessionId { get; set; }
-        public JobProfile[] JobProfiles { get; set; } = {}
+        public JobProfile[] JobProfiles { get; set; } = {};
         public string Title { get; set; } = "National Careers Service - Results";
         public string TraitTitle { get; set; } = "What you told us";
         public string TraitSummaryText { get; set; } = "Because of your answers, we have provided the following job profiles. You can choose to view more results at the bottom of the page.";
