28bfb9f21dee5261cb6634b45261b4c0c29a88ff
Samuel Cotterall <samuelcotterall@gmail.com>
2019-04-11 16:47:22 +0100

    Added new InformationSources page

---
+++ src/Dfc.DiscoverSkillsAndCareers.WebApp/Controllers/InformationSourcesController.cs
@@ -0,0 +1,59 @@
+
+using Dfc.DiscoverSkillsAndCareers.WebApp.Models;
+using Dfc.DiscoverSkillsAndCareers.WebApp.Services;
+using DFC.Common.Standard.Logging;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Threading.Tasks;
+
+namespace Dfc.DiscoverSkillsAndCareers.WebApp.Controllers
+{
+    [Route("information-sources")]
+    public class InformationSourcesController : BaseController
+    {
+        readonly ILogger<InformationSourcesController> Log;
+        readonly ILoggerHelper LoggerHelper;
+        readonly IApiServices ApiServices;
+
+        public InformationSourcesController(
+            ILogger<InformationSourcesController> log,
+            ILoggerHelper loggerHelper,
+            IApiServices apiServices)
+        {
+            Log = log;
+            LoggerHelper = loggerHelper;
+            ApiServices = apiServices;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+           var correlationId = Guid.NewGuid();
+            try
+            {
+                LoggerHelper.LogMethodEnter(Log);
+
+                var sessionId = await TryGetSessionId(Request);
+                if (string.IsNullOrEmpty(sessionId))
+                {
+                    return Redirect("/");
+                }
+
+                var viewName ="InformationSources";
+                var contentName = "informationsources";
+                var model = await ApiServices.GetContentModel<InformationSourcesViewModel>(contentName, correlationId);
+                Response.Cookies.Append("ncs-session-id", sessionId, new Microsoft.AspNetCore.Http.CookieOptions() { Secure = true, HttpOnly = true });
+                return View(viewName, model);
+            }
+            catch (Exception ex)
+            {
+                LoggerHelper.LogException(Log, correlationId, ex);
+                return StatusCode(500);
+            }
+            finally
+            {
+                LoggerHelper.LogMethodExit(Log);
+            }
+        }
+    }
+}
