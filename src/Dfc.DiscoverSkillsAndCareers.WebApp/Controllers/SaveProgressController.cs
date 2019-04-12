958c0df000a1a59a21d080a620fcf47b6f7e6516
Oliver Booth <obooth@preppygreen.com>
2019-04-12 14:06:24 +0100

    UpdateSessionVarsOnViewModel save progress

--- src/Dfc.DiscoverSkillsAndCareers.WebApp/Controllers/SaveProgressController.cs
+++ src/Dfc.DiscoverSkillsAndCareers.WebApp/Controllers/SaveProgressController.cs
@@ -232,12 +232,7 @@ namespace Dfc.DiscoverSkillsAndCareers.WebApp.Controllers
 
                 var model = await ApiServices.GetContentModel<SaveProgressViewModel>("saveprogresspage", correlationId);
                 model.BackLink = "/save-my-progress";
-                var nextQuestionResponse = await ApiServices.NextQuestion(sessionId, correlationId);
-                model.SessionId = sessionId;
-                model.Code = GetDisplayCode(nextQuestionResponse.ReloadCode);
-                model.SessionDate = nextQuestionResponse.StartedDt.ToString("dd MMMM yyyy");
-                model.Status = $"{nextQuestionResponse.RecordedAnswersCount} out of {nextQuestionResponse.MaxQuestionsCount} statements complete";
-
+                await UpdateSessionVarsOnViewModel(model, sessionId, correlationId);
                 Response.Cookies.Append("ncs-session-id", sessionId, new Microsoft.AspNetCore.Http.CookieOptions() { Secure = true, HttpOnly = true });
                 return View("ReferenceNumber", model);
             }
@@ -250,6 +245,16 @@ namespace Dfc.DiscoverSkillsAndCareers.WebApp.Controllers
             {
                 LoggerHelper.LogMethodExit(Log);
             }
+        }
+
+        [NonAction]
+        public async Task UpdateSessionVarsOnViewModel(SaveProgressViewModel model, string sessionId, Guid correlationId)
+        {
+            var nextQuestionResponse = await ApiServices.NextQuestion(sessionId, correlationId);
+            model.SessionId = sessionId;
+            model.Code = GetDisplayCode(nextQuestionResponse.ReloadCode);
+            model.SessionDate = nextQuestionResponse.StartedDt.ToString("dd MMMM yyyy");
+            model.Status = $"{nextQuestionResponse.RecordedAnswersCount} out of {nextQuestionResponse.MaxQuestionsCount} statements complete";
         }
 
         [HttpPost("reference")]
@@ -266,6 +271,7 @@ namespace Dfc.DiscoverSkillsAndCareers.WebApp.Controllers
                     return Redirect("/");
                 }
                 var model = await ApiServices.GetContentModel<SaveProgressViewModel>("saveprogresspage", correlationId);
+                await UpdateSessionVarsOnViewModel(model, sessionId, correlationId);
                 if (string.IsNullOrEmpty(sendSmsRequest.MobileNumber?.Trim()))
                 {
                     model.ErrorMessage = $"You must enter a phone numner";
