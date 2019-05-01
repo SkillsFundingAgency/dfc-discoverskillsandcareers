d9c2e96eeca0a21f02b184b8a2e06bed50125a64
Samuel Cotterall <samuelcotterall@gmail.com>
2019-05-01 09:43:56 +0100

    Changed default headline on results pages

--- src/Dfc.DiscoverSkillsAndCareers.WebApp/Models/ResultsViewModel.cs
+++ src/Dfc.DiscoverSkillsAndCareers.WebApp/Models/ResultsViewModel.cs
@@ -15,7 +15,7 @@ namespace Dfc.DiscoverSkillsAndCareers.WebApp.Models
         public string Title { get; set; } = "National Careers Service - Results";
         public string TraitTitle { get; set; } = "What you told us";
         public string TraitSummaryText { get; set; } = "Because of your answers, we have provided the following job categories. You can choose to view more results at the bottom of the page.";
-        public string Headline { get; set; } = "Job categories you might be suited to";
+        public string Headline { get; set; } = "Your results";
         public string SaveProgressLinkText { get; set; } = "Save my progress";
         public string SeeLinkButtonText { get; set; } = "See job category";
         public string SaveProgressTitle { get; set; } = "Return to this later";
