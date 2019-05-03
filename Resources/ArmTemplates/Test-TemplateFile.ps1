6b91ef471b6589d2cd8004603d05ad00ac5a236e
Nick Graham <34914709+NickGraham101@users.noreply.github.com>
2019-05-03 09:48:19 +0100

    Ncsd 565 add sitefinity settings (#32)
    * Split content func app out from copy loop
    
    * Added suffix to content app resource name
    
    * Modified FunctionAppNames copy to remove content
    
    * Added additional SiteFinity app settings
    
    * Added Values app settings
    
    * Added Azure Search app settings

--- Resources/ArmTemplates/Test-TemplateFile.ps1
+++ Resources/ArmTemplates/Test-TemplateFile.ps1
@@ -1,24 +1,57 @@
-$IsLoggedIn = (Get-AzureRMContext -ErrorAction SilentlyContinue).Account
-if (!$IsLoggedIn) {
-    Login-AzureRmAccount
-}
-elseif ($($IsLoggedIn.Id.split("@")[1] -eq "citizenazuresfabisgov.onmicrosoft.com") -or $($IsLoggedIn.Id.split("@")[1] -eq "fcsazuresfabisgov.onmicrosoft.com")) {
-    throw "Logged in to SFA tenant.  Login to your personal tenant."
-}
+[CmdletBinding()]
+param(
+    [switch]$DeployToOwnTenant
+)
 
-$ResourceGroupName = "dfc-my-skillscareers-rg"
 $Location = "West Europe"
-$ResourceGroup = Get-AzureRmResourceGroup -Name $ResourceGroupName -ErrorAction SilentlyContinue
-if (!$ResourceGroup) {
-    Write-Host "- Creating resource group"
-    New-AzureRmResourceGroup -Name $ResourceGroupName -Location $Location
-}
 
 $DeploymentParameters = @{
-    ResourceGroup         = $ResourceGroupName
     TemplateParameterFile = ".\Resources\ArmTemplates\test-parameters.json"
     TemplateFile          = ".\Resources\ArmTemplates\template.json"
 }
 
-Write-Host "- Deploying template"
-New-AzureRmResourceGroupDeployment @DeploymentParameters
+if($DeployToOwnTenant.IsPresent) {
+
+    $IsLoggedIn = (Get-AzureRMContext -ErrorAction SilentlyContinue).Account
+    if (!$IsLoggedIn) {
+        Login-AzureRmAccount
+    }
+    elseif ($($IsLoggedIn.Id.split("@")[1] -eq "citizenazuresfabisgov.onmicrosoft.com") -or $($IsLoggedIn.Id.split("@")[1] -eq "fcsazuresfabisgov.onmicrosoft.com")) {
+        throw "Logged in to SFA tenant.  Login to your personal tenant."
+    }
+    
+    $TemplateParamsObject = Get-Content $DeploymentParameters['TemplateParameterFile'] | ConvertFrom-Json
+
+    $ResourceGroupName = $TemplateParamsObject.parameters.ApimResourceGroup.value
+    $ResourceGroup = Get-AzureRmResourceGroup -Name $ResourceGroupName -ErrorAction SilentlyContinue
+    if (!$ResourceGroup) {
+        Write-Host "- Creating resource group $ResourceGroupName"
+        New-AzureRmResourceGroup -Name $ResourceGroupName -Location $Location
+    }
+
+    $ResourceGroupName = "dfc-my-skillscareers-rg"
+    $ResourceGroup = Get-AzureRmResourceGroup -Name $ResourceGroupName -ErrorAction SilentlyContinue
+    if (!$ResourceGroup) {
+        Write-Host "- Creating resource group $ResourceGroupName"
+        New-AzureRmResourceGroup -Name $ResourceGroupName -Location $Location
+    }
+    
+    $DeploymentParameters['ResourceGroup'] = $ResourceGroupName
+    $DeploymentParameters['DeploymentDebugLogLevel'] = "All"
+    
+    Write-Host "- Deploying template"
+    New-AzureRmResourceGroupDeployment @DeploymentParameters
+
+} 
+else {
+
+    $ResourceGroupName = "dfc-test-template-rg"
+
+    $DeploymentParameters['ResourceGroup'] = $ResourceGroupName
+    $DeploymentParameters['Verbose'] = $true
+
+    Write-Host "- Validating template"
+    Test-AzureRmResourceGroupDeployment @DeploymentParameters
+    
+}
+
