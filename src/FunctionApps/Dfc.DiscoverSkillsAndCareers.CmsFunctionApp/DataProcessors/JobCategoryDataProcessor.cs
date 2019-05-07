e86cfb606d9b7236129bcc62353000eb179c91af
Oliver Booth <obooth@preppygreen.com>
2019-05-07 15:17:21 +0100

    Remove exisitng job category on cms import if code changes for same title

--- src/FunctionApps/Dfc.DiscoverSkillsAndCareers.CmsFunctionApp/DataProcessors/JobCategoryDataProcessor.cs
+++ src/FunctionApps/Dfc.DiscoverSkillsAndCareers.CmsFunctionApp/DataProcessors/JobCategoryDataProcessor.cs
@@ -36,10 +36,19 @@ namespace Dfc.DiscoverSkillsAndCareers.CmsFunctionApp.DataProcessors
 
             logger.LogInformation($"Have {data?.Count} job Categorys to save");
 
+            var partitionKey = "jobfamily-cms";
             var codes = new List<string>();
             foreach (var jobCategory in data)
             {
                 var code = JobCategoryHelper.GetCode(jobCategory.Title);
+                // Remove any old job categories that have this title but will have a different code
+                var existingJobCategories = await JobCategoryRepository.GetJobCategoriesByName(partitionKey, jobCategory.Title);
+                existingJobCategories = existingJobCategories.Where(x => x.JobFamilyCode != code).ToArray();
+                for (var i = 0; i < existingJobCategories.Count(); i++)
+                {
+                    await JobCategoryRepository.DeleteJobCategory(partitionKey, existingJobCategories[i]);
+                }
+                // Import the new job category
                 await JobCategoryRepository.CreateJobCategory(new JobFamily()
                 {
                     JobFamilyName = jobCategory.Title,
@@ -54,7 +63,7 @@ namespace Dfc.DiscoverSkillsAndCareers.CmsFunctionApp.DataProcessors
                     },
                     TraitCodes = jobCategory.Traits.Select(x => x.ToUpper()).ToArray(),
                     JobFamilyCode = code,
-                    PartitionKey = "jobfamily-cms"
+                    PartitionKey = partitionKey
                 });
                 codes.Add(code);
             }
