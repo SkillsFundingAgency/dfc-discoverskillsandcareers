d8dc624ea923e9f4fd2f382867b35ea812f62f3f
Colin Bull <colinbul@gmail.com>
2019-06-28 16:09:55 +0100

    removing unneeded file

--- src/changefeed/Dfc.DiscoverSkillsAndCareers.ChangedFeed.Db/dbo/Tables/Question_ExcludeJobProfile.sql
+++
@@ -1,7 +0,0 @@
-﻿CREATE TABLE [dbo].[Question_JobProfile] (
-    [Id]         VARCHAR (50)  NOT NULL,
-    [QuestionId] VARCHAR (150) NULL,
-    [JobProfile] VARCHAR (100) NULL,
-    [Included]   BIT 
-);
-
