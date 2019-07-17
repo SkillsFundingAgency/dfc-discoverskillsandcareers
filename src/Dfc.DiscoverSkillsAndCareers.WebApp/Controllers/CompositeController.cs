d3acfcac0d0422cdbc021b709a57c9715dac5601
Colin Bull <colinbul@gmail.com>
2019-07-17 17:15:32 +0100

    Making CSS absolute path DFC-9831

--- src/Dfc.DiscoverSkillsAndCareers.WebApp/Controllers/CompositeController.cs
+++ src/Dfc.DiscoverSkillsAndCareers.WebApp/Controllers/CompositeController.cs
@@ -15,10 +15,10 @@ namespace Dfc.DiscoverSkillsAndCareers.WebApp.Controllers
         {
             var data = new[]
             {
-                $"<link rel=\"preload\" href=\"{ApplyCompositeBasePath("/assets/fonts/light-f38ad40456-v1.woff2")}\" as=\"font\" type=\"font/woff2\" crossorigin />",
-                $"<link rel=\"preload\" href=\"{ApplyCompositeBasePath("/assets/fonts/bold-a2452cb66f-v1.woff2")}\" as=\"font\" type=\"font/woff2\" crossorigin />",
-                $"<link rel=\"stylesheet\" href=\"{ApplyCompositeBasePath("/assets/css/main.css")}\" />",
-                $"<link rel=\"stylesheet\" href=\"{ApplyCompositeBasePath("/assets/css/print.css")}\" media=\"print\" />"
+                $"<link rel=\"preload\" href=\"https://localhost:5003/assets/fonts/light-f38ad40456-v1.woff2\" as=\"font\" type=\"font/woff2\" crossorigin />",
+                $"<link rel=\"preload\" href=\"https://localhost:5003/assets/fonts/bold-a2452cb66f-v1.woff2\" as=\"font\" type=\"font/woff2\" crossorigin />",
+                $"<link rel=\"stylesheet\" href=\"https://localhost:5003/assets/css/main.css\" />",
+                $"<link rel=\"stylesheet\" href=\"https://localhost:5003/assets/css/print.css\" media=\"print\" />"
             };
             
             return new OkObjectResult(String.Join(Environment.NewLine, data));
