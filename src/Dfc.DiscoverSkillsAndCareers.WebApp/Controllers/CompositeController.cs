770eb94f7cc61e792decc25f8215f092775536a1
kaderhashmi <52653766+kaderhashmi@users.noreply.github.com>
2019-07-29 10:04:41 +0100

    Use Url from app settings

--- src/Dfc.DiscoverSkillsAndCareers.WebApp/Controllers/CompositeController.cs
+++ src/Dfc.DiscoverSkillsAndCareers.WebApp/Controllers/CompositeController.cs
@@ -1,20 +1,29 @@
+using Dfc.DiscoverSkillsAndCareers.WebApp.Config;
 using Microsoft.AspNetCore.Mvc;
 using System;
+using Microsoft.Extensions.Options;
 
 namespace Dfc.DiscoverSkillsAndCareers.WebApp.Controllers
 {
     [Route("composite")]
     public class CompositeController : BaseController
     {
+        private readonly IOptions<AppSettings> settings;
+
+        public CompositeController(IOptions<AppSettings> settings)
+        {
+            this.settings = settings;
+        }
+
         [Route("styles/{**article}")]
         public IActionResult Styles(string article)
         {
             var data = new[]
             {
-                $"<link rel=\"preload\" href=\"https://localhost:5003/assets/fonts/light-f38ad40456-v1.woff2\" as=\"font\" type=\"font/woff2\" crossorigin />",
-                $"<link rel=\"preload\" href=\"https://localhost:5003/assets/fonts/bold-a2452cb66f-v1.woff2\" as=\"font\" type=\"font/woff2\" crossorigin />",
-                $"<link rel=\"stylesheet\" href=\"https://localhost:5003/assets/css/main.css\" />",
-                $"<link rel=\"stylesheet\" href=\"https://localhost:5003/assets/css/print.css\" media=\"print\" />"
+                $"<link rel=\"preload\" href=\"{settings?.Value.AssetsCDNUrl}/fonts/light-f38ad40456-v1.woff2\" as=\"font\" type=\"font/woff2\" crossorigin />",
+                $"<link rel=\"preload\" href=\"{settings?.Value.AssetsCDNUrl}/fonts/bold-a2452cb66f-v1.woff2\" as=\"font\" type=\"font/woff2\" crossorigin />",
+                $"<link rel=\"stylesheet\" href=\"{settings?.Value.AssetsCDNUrl}/css/main.css\" />",
+                $"<link rel=\"stylesheet\" href=\"{settings?.Value.AssetsCDNUrl}/css/print.css\" media=\"print\" />"
             };
 
             return new OkObjectResult(String.Join(Environment.NewLine, data));
