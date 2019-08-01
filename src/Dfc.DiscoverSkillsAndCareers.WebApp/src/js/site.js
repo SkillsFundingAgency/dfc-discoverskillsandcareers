9ba31b0fd19a08256965960e88cecf079ddf231b
Colin Bull <colinbul@gmail.com>
2019-08-01 08:22:11 +0100

    Aligning performance analytics expectations as per ticket UMB-572

--- src/Dfc.DiscoverSkillsAndCareers.WebApp/src/js/site.js
+++ src/Dfc.DiscoverSkillsAndCareers.WebApp/src/js/site.js
@@ -1,11 +1,11 @@
 const helpers = require('./modules/helpers.js')
-const analytics = require('./modules/analytics.js')
 const results = require('./modules/results.js')
+const analytics = require('./modules/analytics.js')
 const breadcrumbs = require('./modules/breadcrumbs.js')
 const GOVUKFrontend = require('govuk-frontend')
 
 GOVUKFrontend.initAll()
-//analytics.init()
+analytics.init()
 breadcrumbs.init()
 
 if (helpers.isPage('app-page--results')) {
