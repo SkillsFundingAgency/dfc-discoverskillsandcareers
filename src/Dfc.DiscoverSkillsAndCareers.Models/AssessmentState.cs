SkillsFundingAgency/dfc-discoverskillsandcareers

commit 15568de4b44d7252efd804c9cc9821bcd23d5cd7

Resolved ESLint errors introduced by previous PR (#134)

src/Dfc.DiscoverSkillsAndCareers.WebApp/src/js/modules/results.js
@@ -1,226 +1,226 @@
 var results = (function () {
-    const cookieName = '.dysac-result'
-    const cookieData = getCookie(cookieName)
-    const data = cookieData ? JSON.parse(cookieData) : null
-    function breakArrayIntoGroups(data, maxPerGroup) {
-        var groups = []
-        for (var index = 0; index < data.length; index += maxPerGroup) {
-            groups.push(data.slice(index, index + maxPerGroup))
+  const cookieName = '.dysac-result'
+  const cookieData = getCookie(cookieName)
+  const data = cookieData ? JSON.parse(cookieData) : null
+  function breakArrayIntoGroups (data, maxPerGroup) {
+    var groups = []
+    for (var index = 0; index < data.length; index += maxPerGroup) {
+      groups.push(data.slice(index, index + maxPerGroup))
+    }
+    return groups
+  }
+  function setCookie (name, value, days) {
+    var expires = ''
+    if (days) {
+      var date = new Date()
+      date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000))
+      expires = '; expires=' + date.toUTCString()
+    }
+    document.cookie = name + '=' + (value || '') + expires + '; path=/'
+  }
+  function getCookie (name) {
+    var value = '; ' + document.cookie
+    var parts = value.split('; ' + name + '=')
+    if (parts.length === 2) return parts.pop().split(';').shift()
+  }
+  function findAncestor (el, sel) {
+    while ((el = el.parentElement) && !((el.matches || el.matchesSelector).call(el, sel)));
+    return el
+  }
+  return {
+    cardHeight: function () {
+      const lists = Array.prototype.slice.call(document.getElementsByClassName('app-long-results'))
+      lists.map(list => {
+        const cards = Array.prototype.slice.call(list.getElementsByClassName('app-long-results__item'))
+        var groups = breakArrayIntoGroups(cards, 3)
+        groups.map(group => {
+          var descriptionHeight = 0
+          var salaryHeight = 0
+          group.map(card => {
+            var description = card.getElementsByClassName('result-description')[0]
+            var salary = card.getElementsByClassName('result-detail--salary')[0]
+            description.style.height = 'auto'
+            salary.style.height = 'auto'
+            descriptionHeight = description.offsetHeight > descriptionHeight ? description.offsetHeight : descriptionHeight
+            salaryHeight = salary.offsetHeight > salaryHeight ? salary.offsetHeight : salaryHeight
+          })
+          group.map(card => {
+            var description = card.getElementsByClassName('result-description')[0]
+            var salary = card.getElementsByClassName('result-detail--salary')[0]
+            description.style.height = descriptionHeight + 'px'
+            salary.style.height = salaryHeight + 'px'
+          })
+        })
+      })
+    },
+    short: function () {
+      const resultsList = document.getElementById('app-results-list')
+      const resultsItems = Array.prototype.slice.call(resultsList.children)
+      const other = resultsItems.filter(result => resultsItems.indexOf(result) >= 3)
+
+      var saveState = (state) => {
+        let cookieData = getCookie(cookieName)
+        let data = cookieData ? JSON.parse(cookieData) : {}
+        data['general'] = state
+        setCookie(cookieName, JSON.stringify(data))
+      }
+
+      function showItems (items, titleElement, buttonElement) {
+        items.map(item => {
+          item.style.display = 'block'
+        })
+        titleElement.parentNode.removeChild(titleElement)
+        buttonElement.parentNode.removeChild(buttonElement)
+      }
+
+      if (other.length) {
+        other.map(item => {
+          item.style.display = 'none'
+        })
+
+        const wrapperElement = document.createElement('div')
+        wrapperElement.classList.add('app-results-load-more')
+
+        // "See x other job…" title
+        const titleElement = document.createElement('h2')
+        const titleText = other.length === 1 ? 'See ' + other.length + ' other job category you are suited to' : 'See ' + other.length + ' other job categories you are suited to'
+        titleElement.innerHTML = titleText
+
+        wrapperElement.appendChild(titleElement)
+
+        // "See matches" button
+        const buttonElement = document.createElement('p')
+        const buttonText = '<a class="govuk-link govuk-link--no-visited-state" href="">See matches</a>'
+        buttonElement.innerHTML = buttonText
+
+        wrapperElement.appendChild(buttonElement)
+
+        // Append everything to container
+        resultsList.parentNode.appendChild(wrapperElement)
+
+        if (data && data['general']) {
+          showItems(other, titleElement, buttonElement)
         }
-        return groups
-    }
-    function setCookie(name, value, days) {
-        var expires = ''
-        if (days) {
-            var date = new Date()
-            date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000))
-            expires = '; expires=' + date.toUTCString()
+
+        buttonElement.addEventListener('click', function (event) {
+          event.preventDefault()
+          showItems(other, titleElement, buttonElement)
+          saveState(true)
+          return false
+        })
+      }
+    },
+    long: function () {
+      const resultsLists = Array.prototype.slice.call(document.getElementsByClassName('app-long-results'))
+
+      resultsLists.map(resultsList => {
+        const showButtonElement = document.createElement('a')
+        const hideButtonElement = document.createElement('a')
+        const resultsItems = Array.prototype.slice.call(resultsList.children)
+        const rowLength = 3
+        const showMoreText = 'Show _count more profile'
+        const showLessText = 'Show fewer profiles'
+        const cards = resultsItems.filter(result => {
+          return resultsItems.indexOf(result) >= 3
+        })
+        const numOfCards = cards.length
+
+        // Split cards into groups of three and use cookie to see if we should be showing any
+        const code = findAncestor(resultsList, '.app-results__item').dataset.jobFamilyCode
+        const groups = breakArrayIntoGroups(cards, rowLength)
+        const groupsToShow = data && code ? data[code] : null
+
+        let groupIndex = groupsToShow || 0
+
+        var updateCardHeight = () => {
+          if (document.body.clientWidth >= 768) {
+            this.cardHeight()
+          }
         }
-        document.cookie = name + '=' + (value || '') + expires + '; path=/'
-    }
-    function getCookie(name) {
-        var value = '; ' + document.cookie
-        var parts = value.split('; ' + name + '=')
-        if (parts.length === 2) return parts.pop().split(';').shift()
-    }
-    function findAncestor(el, sel) {
-        while ((el = el.parentElement) && !((el.matches || el.matchesSelector).call(el, sel)));
-        return el
-    }
-    return {
-        cardHeight: function () {
-            const lists = Array.prototype.slice.call(document.getElementsByClassName('app-long-results'))
-            lists.map(list => {
-                const cards = Array.prototype.slice.call(list.getElementsByClassName('app-long-results__item'))
-                var groups = breakArrayIntoGroups(cards, 3)
-                groups.map(group => {
-                    var descriptionHeight = 0
-                    var salaryHeight = 0
-                    group.map(card => {
-                        var description = card.getElementsByClassName('result-description')[0]
-                        var salary = card.getElementsByClassName('result-detail--salary')[0]
-                        description.style.height = 'auto'
-                        salary.style.height = 'auto'
-                        descriptionHeight = description.offsetHeight > descriptionHeight ? description.offsetHeight : descriptionHeight
-                        salaryHeight = salary.offsetHeight > salaryHeight ? salary.offsetHeight : salaryHeight
-                    })
-                    group.map(card => {
-                        var description = card.getElementsByClassName('result-description')[0]
-                        var salary = card.getElementsByClassName('result-detail--salary')[0]
-                        description.style.height = descriptionHeight + 'px'
-                        salary.style.height = salaryHeight + 'px'
-                    })
-                })
+
+        var updateButtons = () => {
+          if (getRemainingCards() > 0) {
+            var amount = Math.min(3, getRemainingCards())
+            showButtonElement.style.display = 'initial'
+            showButtonElement.innerText = showMoreText.replace(/_count/g, amount)
+            if (amount > 1) {
+              showButtonElement.innerText = showButtonElement.innerText + 's'
+            }
+          } else {
+            showButtonElement.style.display = 'none'
+            showButtonElement.innerText = showMoreText
+          }
+          if (groupIndex > 0) {
+            hideButtonElement.style.display = 'initial'
+          } else {
+            hideButtonElement.style.display = 'none'
+          }
+        }
+
+        var saveState = () => {
+          let cookieData = getCookie(cookieName)
+          let data = cookieData ? JSON.parse(cookieData) : {}
+          let code = findAncestor(resultsList, '.app-results__item').dataset.jobFamilyCode
+          data[code] = groupIndex
+          setCookie(cookieName, JSON.stringify(data))
+        }
+
+        var getRemainingCards = () => {
+          return numOfCards - (groupIndex * rowLength)
+        }
+
+        if (groups.length) {
+          groups.map((group, index) => {
+            if (!groupsToShow || (groupsToShow && index >= groupsToShow)) {
+              group.map(el => {
+                el.style.display = 'none'
+              })
+            }
+          })
+
+          const wrapperElement = resultsList.nextElementSibling.children[0].children[0]
+
+          // More button
+          showButtonElement.classList.add('govuk-link', 'govuk-link--no-visited-state')
+          showButtonElement.href = '#'
+          showButtonElement.innerText = showMoreText
+          wrapperElement.appendChild(showButtonElement)
+
+          showButtonElement.addEventListener('click', function (event) {
+            event.preventDefault()
+            groups[groupIndex].map(el => {
+              el.style.display = 'block'
             })
-        },
-        short: function () {
-            const resultsList = document.getElementById('app-results-list')
-            const resultsItems = Array.prototype.slice.call(resultsList.children)
-            const other = resultsItems.filter(result => resultsItems.indexOf(result) >= 3)
+            groupIndex += 1
+            updateButtons()
+            saveState()
+            updateCardHeight()
+            return false
+          })
 
-            var saveState = (state) => {
-                let cookieData = getCookie(cookieName)
-                let data = cookieData ? JSON.parse(cookieData) : {}
-                data['general'] = state
-                setCookie(cookieName, JSON.stringify(data))
-            }
+          // Less button
+          hideButtonElement.classList.add('govuk-link', 'govuk-link--no-visited-state')
+          hideButtonElement.href = '#'
+          hideButtonElement.innerText = showLessText
+          wrapperElement.appendChild(hideButtonElement)
 
-            function showItems(items, titleElement, buttonElement) {
-                items.map(item => {
-                    item.style.display = 'block'
-                })
-                titleElement.parentNode.removeChild(titleElement)
-                buttonElement.parentNode.removeChild(buttonElement)
-            }
-
-            if (other.length) {
-                other.map(item => {
-                    item.style.display = 'none'
-                })
-
-                const wrapperElement = document.createElement('div')
-                wrapperElement.classList.add('app-results-load-more')
-
-                // "See x other job…" title
-                const titleElement = document.createElement('h2')
-                const titleText = other.length === 1 ? 'See ' + other.length + ' other job category you are suited to' : 'See ' + other.length + ' other job categories you are suited to'
-                titleElement.innerHTML = titleText
-
-                wrapperElement.appendChild(titleElement)
-
-                // "See matches" button
-                const buttonElement = document.createElement('p')
-                const buttonText = '<a class="govuk-link govuk-link--no-visited-state" href="">See matches</a>'
-                buttonElement.innerHTML = buttonText
-
-                wrapperElement.appendChild(buttonElement)
-
-                // Append everything to container
-                resultsList.parentNode.appendChild(wrapperElement)
-
-                if (data && data['general']) {
-                    showItems(other, titleElement, buttonElement)
-                }
-
-                buttonElement.addEventListener('click', function (event) {
-                    event.preventDefault()
-                    showItems(other, titleElement, buttonElement)
-                    saveState(true)
-                    return false
-                })
-            }
-        },
-        long: function () {
-            const resultsLists = Array.prototype.slice.call(document.getElementsByClassName('app-long-results'))
-
-            resultsLists.map(resultsList => {
-                const showButtonElement = document.createElement('a')
-                const hideButtonElement = document.createElement('a')
-                const resultsItems = Array.prototype.slice.call(resultsList.children)
-                const rowLength = 3
-                const showMoreText = 'Show _count more profile'
-                const showLessText = 'Show fewer profiles'
-                const cards = resultsItems.filter(result => {
-                    return resultsItems.indexOf(result) >= 3
-                })
-                const numOfCards = cards.length
-
-                // Split cards into groups of three and use cookie to see if we should be showing any
-                const code = findAncestor(resultsList, '.app-results__item').dataset.jobFamilyCode
-                const groups = breakArrayIntoGroups(cards, rowLength)
-                const groupsToShow = data && code ? data[code] : null
-
-                let groupIndex = groupsToShow || 0
-
-                var updateCardHeight = () => {
-                    if (document.body.clientWidth >= 768) {
-                        this.cardHeight()
-                    }
-                }
-
-                var updateButtons = () => {
-                    if (getRemainingCards() > 0) {
-                        var amount = Math.min(3, getRemainingCards())
-                        showButtonElement.style.display = 'initial'
-                        showButtonElement.innerText = showMoreText.replace(/_count/g, amount)
-                        if (amount > 1) {
-                            showButtonElement.innerText = showButtonElement.innerText + 's'
-                        }
-                    } else {
-                        showButtonElement.style.display = 'none'
-                        showButtonElement.innerText = showMoreText
-                    }
-                    if (groupIndex > 0) {
-                        hideButtonElement.style.display = 'initial'
-                    } else {
-                        hideButtonElement.style.display = 'none'
-                    }
-                }
-
-                var saveState = () => {
-                    let cookieData = getCookie(cookieName)
-                    let data = cookieData ? JSON.parse(cookieData) : {}
-                    let code = findAncestor(resultsList, '.app-results__item').dataset.jobFamilyCode
-                    data[code] = groupIndex
-                    setCookie(cookieName, JSON.stringify(data))
-                }
-
-                var getRemainingCards = () => {
-                    return numOfCards - (groupIndex * rowLength)
-                }
-
-                if (groups.length) {
-                    groups.map((group, index) => {
-                        if (!groupsToShow || (groupsToShow && index >= groupsToShow)) {
-                            group.map(el => {
-                                el.style.display = 'none'
-                            })
-                        }
-                    })
-
-                    const wrapperElement = resultsList.nextElementSibling.children[0].children[0]
-
-                    // More button
-                    showButtonElement.classList.add('govuk-link', 'govuk-link--no-visited-state')
-                    showButtonElement.href = '#'
-                    showButtonElement.innerText = showMoreText
-                    wrapperElement.appendChild(showButtonElement)
-
-                    showButtonElement.addEventListener('click', function (event) {
-                        event.preventDefault()
-                        groups[groupIndex].map(el => {
-                            el.style.display = 'block'
-                        })
-                        groupIndex += 1
-                        updateButtons()
-                        saveState()
-                        updateCardHeight()
-                        return false
-                    })
-
-                    // Less button
-                    hideButtonElement.classList.add('govuk-link', 'govuk-link--no-visited-state')
-                    hideButtonElement.href = '#'
-                    hideButtonElement.innerText = showLessText
-                    wrapperElement.appendChild(hideButtonElement)
-
-                    hideButtonElement.addEventListener('click', function (event) {
-                        event.preventDefault()
-                        groups[groupIndex - 1].map(el => {
-                            el.style.display = 'none'
-                        })
-                        groupIndex -= 1
-                        updateButtons()
-                        saveState()
-                        updateCardHeight()
-                        return false
-                    })
-                }
-
-                updateButtons()
+          hideButtonElement.addEventListener('click', function (event) {
+            event.preventDefault()
+            groups[groupIndex - 1].map(el => {
+              el.style.display = 'none'
             })
+            groupIndex -= 1
+            updateButtons()
+            saveState()
+            updateCardHeight()
+            return false
+          })
         }
+
+        updateButtons()
+      })
     }
+  }
 })()
 
 module.exports = results

commit db0af960b34feff7a10b825a4b45697c9b7b12d7

Merge Develop - Fixes for DFC-10073 and DFC-9987 (#139)
* Fixing change feed using incorrect properties since update UMB-582

* Updated JMX script for domain change

* Updated JMX script for domain change (#131)

* Fix IE11 issue where show more link not rendered due to classlist in strict mode (#132)

* Bug fix/dfc 10123 back button still shown (#133)

* Updated JMX script for domain change

* Removed back button from first results page DFC-10123, also added missing robots meta tag "noindex, nofollow"

* Resolved ESLint errors introduced by previous PR (#134)

* Updated reference code formatting to be consistent in site, e-mail an… (#135)

* Updated reference code formatting to be consistent in site, e-mail and sms. Fixes DFC-10211

* Adding missed updated file.

* Fixes for DFC-9987 (#137)

* Fix Show Fewer profiles in IE11 (#138)

src/Dfc.DiscoverSkillsAndCareers.Models/AssessmentState.cs
@@ -32,7 +32,7 @@ namespace Dfc.DiscoverSkillsAndCareers.Models
         {
             get
             {
-                var complete = (RecordedAnswers.Length >= MaxQuestions && CurrentQuestion == MaxQuestions);
+                var complete = RecordedAnswers.Length >= MaxQuestions;
 
                 if (complete && !CompleteDt.HasValue)
                 {

src/Dfc.DiscoverSkillsAndCareers.WebApp/Controllers/QuestionController.cs
@@ -141,6 +141,17 @@ namespace Dfc.DiscoverSkillsAndCareers.WebApp.Controllers
                     throw new Exception($"Question not found for session {sessionId}");
                 }
 
+                if (!invalidAnswer
+                    && !nextQuestionResponse.IsComplete && questionNumber != nextQuestionResponse.NextQuestionNumber)
+                {
+                    return Redirect($"/q/{assessment}/{nextQuestionResponse.NextQuestionNumber?.ToQuestionPageNumber()}");
+                }
+
+                if (!invalidAnswer && nextQuestionResponse.IsComplete)
+                {
+                    var finishEndpoint = nextQuestionResponse.IsFilterAssessment ? $"/results/{nextQuestionResponse.JobCategorySafeUrl}" : "/results";
+                    return Redirect(finishEndpoint);
+                }
                 
                 var formRoute = GetAnswerFormPostRoute(nextQuestionResponse, assessment);
                 int displayPercentComplete = nextQuestionResponse.PercentComplete;

src/Dfc.DiscoverSkillsAndCareers.WebApp/src/js/modules/results.js
@@ -151,7 +151,7 @@ var results = (function () {
             showButtonElement.innerText = showMoreText
           }
           if (groupIndex > 0) {
-            hideButtonElement.style.display = 'initial'
+            hideButtonElement.style.display = 'inline'
           } else {
             hideButtonElement.style.display = 'none'
           }

src/Dfc.DiscoverSkillsAndCareers.UnitTests/ControllerTests/QuestionControllerTests.cs
@@ -260,7 +260,8 @@ namespace Dfc.UnitTests.ControllerTests
                 new AssessmentQuestionResponse
                 {
                     SessionId = "Abc123",
-                    QuestionNumber = 1
+                    QuestionNumber = 1,
+                    NextQuestionNumber = 1
                 }));
 
             var result = await _controller.NewAssessment("short");
@@ -278,7 +279,8 @@ namespace Dfc.UnitTests.ControllerTests
             _apiServices.Question("Abc123", "short", 1, Arg.Any<Guid>())
                 .Returns(Task.FromResult(new AssessmentQuestionResponse
                 {
-                    IsFilterAssessment = true
+                    IsFilterAssessment = true,
+                    NextQuestionNumber = 1
                 }));
             
             var result = await _controller.NextQuestion("Abc123","short", 1, false);
@@ -296,7 +298,8 @@ namespace Dfc.UnitTests.ControllerTests
             _apiServices.Question("Abc123", "short", 1, Arg.Any<Guid>())
                 .Returns(Task.FromResult(new AssessmentQuestionResponse
                 {
-                    IsFilterAssessment = false
+                    IsFilterAssessment = false,
+                    NextQuestionNumber = 1
                 }));
             
             var result = await _controller.NextQuestion("Abc123","short", 1, false);