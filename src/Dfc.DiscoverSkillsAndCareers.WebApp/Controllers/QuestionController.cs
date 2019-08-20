d600a5193587d6827a9ac8a5c34770246850b31d
Colin <colinbull@users.noreply.github.com>
2019-08-20 08:22:04 +0100

    Fixes for DFC-9987 (#137)

--- src/Dfc.DiscoverSkillsAndCareers.WebApp/Controllers/QuestionController.cs
+++ src/Dfc.DiscoverSkillsAndCareers.WebApp/Controllers/QuestionController.cs
@@ -141,6 +141,17 @@ namespace Dfc.DiscoverSkillsAndCareers.WebApp.Controllers
                     throw new Exception($"Question not found for session {sessionId}");
                 }
 
+                if (!invalidAnswer
+                    && !nextQuestionResponse.IsComplete && questionNumber != nextQuestionResponse.NextQuestionNumber)
+                {
+                    return Redirect($"/q/{assessment}/{nextQuestionResponse.NextQuestionNumber?.ToQuestionPageNumber()}");
+                }
+
+                if (!invalidAnswer && nextQuestionResponse.IsComplete)
+                {
+                    var finishEndpoint = nextQuestionResponse.IsFilterAssessment ? $"/results/{nextQuestionResponse.JobCategorySafeUrl}" : "/results";
+                    return Redirect(finishEndpoint);
+                }
                 
                 var formRoute = GetAnswerFormPostRoute(nextQuestionResponse, assessment);
                 int displayPercentComplete = nextQuestionResponse.PercentComplete;
